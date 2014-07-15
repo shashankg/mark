0396fe107c307bfff6be06bd8d89d96345962865
shefali <shefali.jain87@gmail.com>
2014-07-15 23:29:15 +0530

    add and remove from basket

--- src/main/java/com/mark/resource/page/ShoppingBasketPage.java
+++ src/main/java/com/mark/resource/page/ShoppingBasketPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ShoppingBasketPage extends BasePage<ShoppingBasketPage> {
     private static final String CHECKOUT_NOW_CLASS = "checkout";
-    private static final String REMOVE_ITEM_ID = "";
+    private static final String REMOVE_ITEM_XPATH = ".//*[@id='154595059']/span[2]/a";
 
     /**
      * Constructor
@@ -18,6 +18,13 @@ public class ShoppingBasketPage extends BasePage<ShoppingBasketPage> {
     public ShoppingBasketPage(WebDriver driver) {
         super(driver);
     }
+
+//    public removeEmptiesCart()
+//    {
+//        getWebElement(By.xpath("REMOVE_ITEM_XPATH")).click();
+//
+//    }
+
 
     @Override
     protected ExpectedCondition getPageLoadCondition() {
--- src/test/java/com/mark/ExampleTest.java
+++ src/test/java/com/mark/ExampleTest.java
@@ -1,4 +1,5 @@
 package com.mark;
+import com.sun.swing.internal.plaf.synth.resources.synth_sv;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,7 +14,8 @@ public class ExampleTest {
 
     public static void main(String args[]) {
         WebDriver driver = new FirefoxDriver();
-        String baseURL = "http://www.landmarkshops.com/";
+       // String baseURL = "http://www.landmarkshops.com/";
+        String baseURL = "http://www.landmarkshops.com/Electronics/Electronics/Audio/Headphone-%26-Earphones/Earbud-%26-In-Ear-Headphones/Sennheiser-CX-300-II-Earphones---Chrome/p/154007163";
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
@@ -79,68 +81,90 @@ public class ExampleTest {
 //    driver.findElement(By.id("addToBasket")).click();
 
 
-        String str = driver.getTitle();
-        System.out.print(driver.getTitle());
-        Assert.assertEquals(str,"Online Shopping for Clothing, Baby Products & Electronics at LandmarkShops.com");
-        Actions build = new Actions(driver);
-        WebElement hover = driver.findElement(By.xpath(".//*[@id='electronics']/a"));
-        build.moveToElement(hover).build().perform();
-        driver.findElement(By.linkText("Mobile Phones")).click();
-        driver.findElement(By.xpath(".//*[@id='main']/div/div[3]/div[1]/div[3]/div[2]/div[1]/div/a/img")).click();
-        //String price1 = driver.findElement(By.xpath(".//*[@id='main']/div/div[3]/div[1]/div[3]/div[1]/div[2]/div/span[1]/strong")).getText();
-        //String price2 = driver.findElement(By.xpath(".//*[@id='productPriceBlock']/strong")).getText();
-        //Assert.assertEquals(price1,price2);
-        //driver.findElement(By.id("sizeSelect8")).click();
+        //String str = driver.getTitle();
+        //System.out.print(driver.getTitle());
+        //Assert.assertEquals(str,"Online Shopping for Clothing, Baby Products & Electronics at LandmarkShops.com");
+        //Actions build = new Actions(driver);
+        //WebElement hover = driver.findElement(By.xpath(".//*[@id='electronics']/a"));
+        //build.moveToElement(hover).build().perform();
+        //driver.findElement(By.linkText("Mobile Phones")).click();
+        //driver.findElement(By.xpath(".//*[@id='main']/div/div[3]/div[1]/div[3]/div[2]/div[1]/div/a/img")).click();
+//        //String price1 = driver.findElement(By.xpath(".//*[@id='main']/div/div[3]/div[1]/div[3]/div[1]/div[2]/div/span[1]/strong")).getText();
+//        //String price2 = driver.findElement(By.xpath(".//*[@id='productPriceBlock']/strong")).getText();
+//        //Assert.assertEquals(price1,price2);
+//        //driver.findElement(By.id("sizeSelect8")).click();
         driver.findElement(By.id("addToBasket")).click();
-        //driver.findElement(By.className("basket btn-open")).click();
-        //driver.findElement(By.xpath(".//*[@id='wrap-basket-template']/a")).click();
-        driver.findElement(By.className("checkout")).click();
-        driver.findElement(By.id("addressNickName")).sendKeys("test");
-        driver.findElement(By.id("firstName")).sendKeys("test");
-        driver.findElement(By.id("lastName")).sendKeys("test");
-        driver.findElement(By.id("emaill")).sendKeys("test@test.com");
-        driver.findElement(By.id("line1")).sendKeys("test");
-        driver.findElement(By.id("line2")).sendKeys("test");
-        WebElement regions=driver.findElement(By.id("regions"));
-    List<WebElement> ddownregionOption=regions.findElements(By.tagName("option"));
+//        //driver.findElement(By.className("basket btn-open")).click();
+//        //driver.findElement(By.xpath(".//*[@id='wrap-basket-template']/a")).click();
+//        driver.findElement(By.className("checkout")).click();
+//        driver.findElement(By.id("addressNickName")).sendKeys("test");
+//        driver.findElement(By.id("firstName")).sendKeys("test");
+//        driver.findElement(By.id("lastName")).sendKeys("test");
+//        driver.findElement(By.id("emaill")).sendKeys("test@test.com");
+//        driver.findElement(By.id("line1")).sendKeys("test");
+//        driver.findElement(By.id("line2")).sendKeys("test");
+//        WebElement regions=driver.findElement(By.id("regions"));
+//    List<WebElement> ddownregionOption=regions.findElements(By.tagName("option"));
+//
+//    for(WebElement value:ddownregionOption){
+//        if(value.getText().equalsIgnoreCase("Dubai")){
+//            value.click();
+//        }   }
+//
+//        driver.findElement(By.id("phone")).sendKeys("123456789");
+//        driver.findElement(By.xpath(".//*[@id='createLMGAddressForm']/div[2]/input")).click();
+//        driver.findElement(By.id("sameBillingAsShipping")).click();
+//
+//
+//
+//        driver.findElement(By.id("ccf-number")).sendKeys("5566204900066815");
+//        driver.findElement(By.id("ccf-name")).sendKeys("test");
+//
+//        WebElement expirymonth=driver.findElement(By.id("ccf-exp-month"));
+//        List<WebElement> ddownexpirymonth=expirymonth.findElements(By.tagName("option"));
+//
+//        for(WebElement value:ddownexpirymonth){
+//            if(value.getText().equalsIgnoreCase("05")){
+//                value.click();
+//            }   }
+//
+//        WebElement expiryyear=driver.findElement(By.id("ccf-exp-year"));
+//        List<WebElement> ddownexpiryyear=expiryyear.findElements(By.tagName("option"));
+//
+//        for(WebElement value:ddownexpiryyear){
+//            if(value.getText().equalsIgnoreCase("2022")){
+//                value.click();
+//            }   }
+//
+//        driver.findElement(By.id("ccf-cvv")).sendKeys("111");
+//        driver.findElement(By.xpath(".//*[@id='createLMGAddressForm']/div[7]/input[5]")).click();
+//        driver.findElement(By.id("submitBtn2")).click();
+//        //driver.findElement(By.xpath("html/body/form/table/tbody/tr[6]/td[2]/input[1]")).click();
+//        driver.findElement(By.id("cmdSubmit")).click();
+//        driver.switchTo().alert().accept();
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+        driver.findElement(By.xpath(".//*[@id='wrap-basket-template']/a")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //driver.findElement(By.xpath(".//*[@id='154595059']/span[2]/a")).click();
+        driver.findElement(By.className("deleteRowButton")).click();
+       //WebElement str = driver.findElement(By.xpath(".//*[@id='wrap-basket-template']/a/strong/span"));
+        driver.navigate().refresh();
+        WebElement str = driver.findElement(By.className("basket-count"));
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+        System.out.println(str.getText());
 
-    for(WebElement value:ddownregionOption){
-        if(value.getText().equalsIgnoreCase("Dubai")){
-            value.click();
-        }   }
+        if(str.getText().contains("0"))
 
-        driver.findElement(By.id("phone")).sendKeys("123456789");
-        driver.findElement(By.xpath(".//*[@id='createLMGAddressForm']/div[2]/input")).click();
-        driver.findElement(By.id("sameBillingAsShipping")).click();
+        {
+           System.out.print("Item Removed");
+        }
 
 
-
-        driver.findElement(By.id("ccf-number")).sendKeys("5566204900066815");
-        driver.findElement(By.id("ccf-name")).sendKeys("test");
-
-        WebElement expirymonth=driver.findElement(By.id("ccf-exp-month"));
-        List<WebElement> ddownexpirymonth=expirymonth.findElements(By.tagName("option"));
-
-        for(WebElement value:ddownexpirymonth){
-            if(value.getText().equalsIgnoreCase("05")){
-                value.click();
-            }   }
-
-        WebElement expiryyear=driver.findElement(By.id("ccf-exp-year"));
-        List<WebElement> ddownexpiryyear=expiryyear.findElements(By.tagName("option"));
-
-        for(WebElement value:ddownexpiryyear){
-            if(value.getText().equalsIgnoreCase("2022")){
-                value.click();
-            }   }
-
-        driver.findElement(By.id("ccf-cvv")).sendKeys("111");
-        driver.findElement(By.xpath(".//*[@id='createLMGAddressForm']/div[7]/input[5]")).click();
-        driver.findElement(By.id("submitBtn2")).click();
-        //driver.findElement(By.xpath("html/body/form/table/tbody/tr[6]/td[2]/input[1]")).click();
-        driver.findElement(By.id("cmdSubmit")).click();
-        driver.switchTo().alert().accept();
-
+       //driver.close();
 
     }
 }
